5ed6806bc730fea7da49dce317cc5813d7f20326
zaiverse <brenda.garcia007@mymdc.net>
2022-12-23 10:23:51 -0800

    Playing with Qt

--- app.py
+++ app.py
@@ -1,49 +1,21 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFrame, QGridLayout
 
 class Practice(QWidget):
 
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.grid = QGridLayout()
 
-        self.layout = QVBoxLayout()
-        self.horizontal = QHBoxLayout()
+        self.createButton("Hello",1,1)
+        self.createButton("yikes",1,2)
+        self.setLayout(self.grid)
 
-        self.setLayout(self.layout)
+    def createButton(self,text,col,row):
 
-        self.frame1()
-
-    def frame1(self):   
-        firstVecticalCol = QVBoxLayout()
-        secondVecticalCol = QVBoxLayout()
-        thirdVecticalCol = QVBoxLayout()
-
-        print("IN here!")
-        button1 = QPushButton("^")
-        firstVecticalCol.addWidget(button1)
-
-        button2 = QPushButton("-")
-        firstVecticalCol.addWidget(button2)
-
-        button3 = QPushButton("^")
-        secondVecticalCol.addWidget(button3)
-
-        button4 = QPushButton("-")
-        secondVecticalCol.addWidget(button4)
-
-        button5 = QPushButton("^")
-        thirdVecticalCol.addWidget(button5)
-
-        button6 = QPushButton("-")
-        thirdVecticalCol.addWidget(button6)
-
-        self.horizontal.addLayout(firstVecticalCol)
-        self.horizontal.addLayout(secondVecticalCol)
-        self.horizontal.addLayout(thirdVecticalCol)
-        self.layout.addLayout(self.horizontal)
-
+        button = QPushButton(text)
+        self.grid.addWidget(button,col,row)
 
 
 
